c7084d91b17e267218b6f3daa3f3e6acd97114f6
AshutoshRawat2998 <104488086+AshutoshRawat2998@users.noreply.github.com>
2022-04-28 16:36:13 +0530

    First commit

---
+++ dom1.js
@@ -0,0 +1,19 @@
+// GETELEMENTBYTAGNAME
+
+var li = document.getElementsByTagName('li');
+console.log(li);
+li[1].textContent = 'Ashutosh';
+li[1].style.fontWeight = 'bold';
+li[1].style.backgroundColor = 'yellow';
+
+
+// GETELEMENTSBYCLASSNAME
+
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// items[1].textContent = "Ashutosh";
+// items[1].style.fontWeight = 'bold';
+// items[1].style.backgroundColor = 'yellow';
+
+
+
